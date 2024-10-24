be3e27564abc452844d1b1c00352c0f2ea76d29e
mbaddah <mabaddah@gmail.com>
2024-10-25 06:03:36 +1000

    Further refactoring

--- app/rtsw.py
+++ app/rtsw.py
@@ -1,34 +1,18 @@
-from datetime import datetime
+import logging
 import utils.db_config as db_config
 import utils.kyoto_dst_request as kyoto_dst_request
 
+logging.basicConfig(level=logging.INFO)
+
 def main():
     # Fetch the data
-    data = kyoto_dst_request.request_kyoto_dst()
-    print(data)
+    data = kyoto_dst_request.fetch_kyoto_dst()
     
     # Connect to the MySQL database
     db = db_config.get_db_connection()
     cursor = db.cursor()
-
-    # Skip the header row and parse the JSON data to insert into the database
-    for entry in data[1:]:
-        try:
-            time_str = entry[0]
-            value = float(entry[1])
-            
-            # Convert time string to a datetime object
-            time_obj = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
-            
-            # Insert the data into the MySQL table
-            cursor.execute("""
-            INSERT INTO kyoto_dst (time_tag, dst)
-            VALUES (%s, %s)
-            """, (time_obj, value))
-        except IndexError as e:
-            print(f"Skipping entry due to missing fields: {entry}")
-        except ValueError as e:
-            print(f"Skipping entry due to value error: {entry} - {e}")
+    
+    db_config.insert_dst(cursor, data)
 
     # Commit the transaction
     db.commit()
@@ -37,7 +21,8 @@ def main():
     cursor.close()
     db.close()
 
-    print("Data inserted into MySQL database")
+    logging.info("Sucessfully inserted data into the database")
+
 
 if __name__ == "__main__":
     main()
--- app/utils/db_config.py
+++ app/utils/db_config.py
@@ -1,6 +1,9 @@
 import mysql.connector
 import os
 
+from datetime import datetime
+
+
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv('DB_HOST', 'mysql'),
@@ -9,3 +12,22 @@ def get_db_connection():
         password=os.getenv('DB_PASSWORD', 'password'),
         database=os.getenv('DB_NAME', 'dst_data')
     )
+
+def insert_dst(cursor, data):
+    for entry in data[1:]:
+        try:
+            time_str = entry[0]
+            value = float(entry[1])
+            
+            # Convert time string to a datetime object
+            time_obj = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+            
+            # Insert the data into the MySQL table
+            cursor.execute("""
+            INSERT INTO kyoto_dst (time_tag, dst)
+            VALUES (%s, %s)
+            """, (time_obj, value))
+        except IndexError:
+            print(f"Skipping entry due to missing fields: {entry}")
+        except ValueError as e:
+            print(f"Skipping entry due to value error: {entry} - {e}")
